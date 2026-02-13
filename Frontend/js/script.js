3a655403f3844863a01a3fc30090c172dd2f2cc3
zroave <ar5849119@gmail.com>
2026-02-13 22:01:31 +0530

    Connected frontend to production backend

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://triptales-rvym.onrender.com/api";
+const API_BASE = "https://triptales-rvym.onrender.com";
 
 
 const getToken = () => localStorage.getItem('tt_token') || '';
